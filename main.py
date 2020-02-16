83b07372fe7f967d13b75b3a5f342fa8cb8d9cf2
Aarón Gama <25625929+ElWakamolexD@users.noreply.github.com>
2020-02-15 21:59:27 -0600

    v.0.0.2
    El programa se dividió en funciones

--- main.py
+++ main.py
@@ -7,7 +7,34 @@ from collections import Counter
 #Variables
 keyWords = ["if","while","for","do while"]
 fileContent = 0
-count = 0
+auxFile = 0
+path = 0
+files = []
+
+#Funciones
+#DIP (list,int,string)
+#file es una lista resultado de hacer split al contenido del archivo
+#depth es un contador de la altura del árbol
+#fileExt es la extensión del archivo para abrir el siguiente
+def DIP(file,depth,fileExt): 
+	#¿El arreglo contiene la palabra extends?
+	if 'extends' in file: #Si
+		depth = depth + 1 #Aumentamos el contador 
+		#Obtenemos el nombre del siguiente archivo
+		#join(path,file.find('extends')+1,fileExt)
+		auxFile = open(join(path,file[file.index('extends')+1],fileExt),"r").read()
+		#auxFile es el contenido del siguiente archivo
+		return DIP(auxFile.split(),depth,fileExt)
+	else:
+		return depth
+
+#WMC(string)
+#file es un String con todo el contenido del archivo
+def WMC(file): 
+	counter = 0 
+	for word in keyWords: #Verificamos que el archivo contenga las palabras almacenadas en keywords
+		counter = counter + file.count(word)
+	return counter
 
 print("Bienvenido al analizador de archivos")
 path = input("Ingresa la ruta a la carpeta contenedora de archivos:\n>")
@@ -19,7 +46,8 @@ for file in files:
 	except Exception:
 		print("Error")
 		continue
-	for word in keyWords:
-		count = count + fileContent.count(word)
-	print("In file "+file+" WMC="+str(count))
-	count = 0
+	#print("In file "+file+" WMC="+str(WMC(fileContent)))
+	try:
+		print("In file "+file+" DIP="+str(DIP(fileContent.split(),0,file[file.find('.'):])))
+	except FileNotFoundError:
+		print("El archivo "+file+" extiende de una clase ubicada en una libreria")
