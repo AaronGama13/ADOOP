3189e91f5e4bf92390e95ac67f9356c52a841889
Aarón Gama <25625929+ElWakamolexD@users.noreply.github.com>
2020-02-18 22:36:04 -0600

    v.0.0.6
    Ya quedó el último

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ auxFile = 0
 path = 0
 files = []
 
-
 #Funciones
+def NOC(file):
+	auxFile = open(join(path,file),"r").read() #Leemos el contenido del archivo
+	for x in auxFile[auxFile.find('extends'):auxFile.find('{')].split():
+		if x != 'extends' and x != 'implements' and x != ',':
+			if x not in nocs.keys():
+				nocs[x] = 1
+			else:
+				nocs[x] = nocs.get(x) + 1
+		
+
 def DIP(file,height):
 	if exists(join(path,file)): #Si el archivo existe en la carpeta indicada
 		auxFile = open(join(path,file),"r").read() #Leemos el contenido del archivo
@@ -36,6 +45,7 @@ print("Bienvenido al analizador de archivos")
 path = input("Ingresa la ruta a la carpeta contenedora de archivos:\n>")
 #Creando un arreglo de puros archivos, la función isfile verifica que ruta+nombre es un archivo
 files = [file for file in listdir(path) if isfile(join(path,file))]  
+nocs = dict.fromkeys([files[0]],0)
 for file in files:
 	try:
 		fileContent = open(join(path,file),"r").read()
@@ -44,6 +54,8 @@ for file in files:
 		continue
 	try:
 		#print("In file "+file+" WMC="+str(WMC(fileContent)))
-		print("In file "+file+" DIP="+str(DIP(file,0)))
+		#print("In file "+file+" DIP="+str(DIP(file,0)))
+		NOC(file)
 	except Exception as Err:
 		print(Err)
+print(nocs)
