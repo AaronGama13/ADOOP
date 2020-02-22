d4db6e9b97fdb1dd485a85db547466d14f6a901a
Aarón Gama <25625929+ElWakamolexD@users.noreply.github.com>
2020-02-21 19:36:15 -0600

    v.0.0.7
    Versión orientada a obejtos

---
+++ analyzer.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# AUTORES: García Medina Saúl, Palacios Lugo Alan Yoltic,
+#		   Sánchez Castro Aarón Gamaliel.
+# DESCRIPCIÓN
+# FECHA: 14/02/2020. Todos los derechos reservados
+
+from os.path import join, exists
+
+class Analyzer:
+
+	#Palabras a ubicar en un archivo para el método MWC
+	keyWords = ["if","while","for","switch,"]
+	#Ruta a la carpeta que contiene todos los archivos a analizar
+	path = None
+	#Diccionario que contiene todas las clases con su número de hijos
+	nocs = None
+
+	#Constructor de la clase Analyzer, no requiere parámetros para inicialización
+	def __init__(self,pathFolder,firstFile):
+		self.path = pathFolder
+		print(firstFile)
+		self.nocs = dict.fromkeys([firstFile],0)
+
+	#WMC (Weighted Method per Class)
+	#Ésta función recibe un archivo (file) lo convierte en cadena y busca 
+	#coincidencias con cada palabra reservada ubicada en el arreglo keyWords
+	def wmc(self,file): 
+		counter = 0
+		auxFile = open(join(self.path,file),"r").read()
+		for word in self.keyWords: #Verificamos que el archivo contenga las palabras almacenadas en keywords
+			counter = counter + auxFile.count(word)
+		return counter
+
+	#DIT (Depth Inheritance Tree)
+	#Ésta función realiza un conteo recursivo por cada archivo recibido (file)
+	#retorna la altura más alta alcanzada por el árbol de ascendencia
+	def dit(self,file,height):
+		if exists(join(self.path,file)): #Si el archivo existe en la carpeta indicada
+			auxFile = open(join(self.path,file),"r").read() #Leemos el contenido del archivo
+			parents = [] #Todos los padres aquí jiji
+			for x in auxFile[auxFile.find('extends'):auxFile.find('{')].split():
+				if x != 'extends' and x != 'implements' and x != ',':
+					parents.append(x+file[file.find('.'):]) #Si la cadena es una clase entonces agregala
+			for p in parents:
+				return self.dit(p,height+1)
+		else:
+			return height
+
+	#NOC (Number Of Children)
+	def noc(self,file):
+		auxFile = open(join(self.path,file),"r").read() #Leemos el contenido del archivo
+		for x in auxFile[auxFile.find('extends'):auxFile.find('{')].split():
+			if x != 'extends' and x != 'implements' and x != ',':
+				if x not in self.nocs.keys():
+					self.nocs[x] = 1
+				else:
+					self.nocs[x] = self.nocs.get(x) + 1
--- main.py
+++ main.py
@@ -1,61 +1,27 @@
 # -*- coding: utf-8 -*-
-import sys # ello syster
-from os import listdir #Ésta librería nos permite listar los archivos contenidos en un dir.
-from os.path import isfile, join, exists
+# AUTORES: García Medina Saúl, Palacios Lugo Alan Yoltic,
+#		   Sánchez Castro Aarón Gamaliel.
+# DESCRIPCIÓN
+# FECHA: 14/02/2020. Todos los derechos reservados
 
-#Variables
-keyWords = ["if","while","for","switch,"]
-fileContent = 0
-auxFile = 0
-path = 0
-files = []
-
-#Funciones
-def NOC(file):
-	auxFile = open(join(path,file),"r").read() #Leemos el contenido del archivo
-	for x in auxFile[auxFile.find('extends'):auxFile.find('{')].split():
-		if x != 'extends' and x != 'implements' and x != ',':
-			if x not in nocs.keys():
-				nocs[x] = 1
-			else:
-				nocs[x] = nocs.get(x) + 1
-		
-
-def DIP(file,height):
-	if exists(join(path,file)): #Si el archivo existe en la carpeta indicada
-		auxFile = open(join(path,file),"r").read() #Leemos el contenido del archivo
-		parents = [] #Todos los padres aquí jiji
-		for x in auxFile[auxFile.find('extends'):auxFile.find('{')].split():
-			if x != 'extends' and x != 'implements' and x != ',':
-				parents.append(x+file[file.find('.'):]) #Si la cadena es una clase entonces agregala
-		for p in parents:
-			return DIP(p,height+1)
-	else:
-		return height
-
-#WMC(string)
-#file es un String con todo el contenido del archivo
-def WMC(file): 
-	counter = 0 
-	for word in keyWords: #Verificamos que el archivo contenga las palabras almacenadas en keywords
-		counter = counter + file.count(word)
-	return counter
+#Ésta librería nos permite revisar las excepciones que se presenten
+import sys
+#Ésta librería nos permite listar los archivos contenidos en un dir.
+from os import listdir 
+#Ésta librería permite verificar si una ruta no es un directorio
+from os.path import isfile, join
+from analyzer import Analyzer
 
 print("Bienvenido al analizador de archivos")
 path = input("Ingresa la ruta a la carpeta contenedora de archivos:\n>")
 #Creando un arreglo de puros archivos, la función isfile verifica que ruta+nombre es un archivo
 files = [file for file in listdir(path) if isfile(join(path,file))]  
-nocs = dict.fromkeys([files[0]],0)
+#Creando un objeto de la clase Analyzer
+myAnalizer = Analyzer(path,files[0][:files[0].find('.')])
+
 for file in files:
-	try:
-		fileContent = open(join(path,file),"r").read()
-	except Exception:
-		print("Error")
-		continue
-	try:
-		#print("In file "+file+" WMC="+str(WMC(fileContent)))
-		#print("In file "+file+" DIP="+str(DIP(file,0)))
-		NOC(file)
-	except Exception as Err:
-		print(Err)
-print(nocs)
+	print("In file "+file+" WCM="+str(myAnalizer.wmc(file)))
+	print("In file "+file+" DIT="+str(myAnalizer.dit(file,0)))
+	myAnalizer.noc(file)
+print("NOC:")
+print(myAnalizer.nocs)
